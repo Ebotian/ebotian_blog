eb3d2ed678b076a5bda69a7ffd0671be35dab09b
EbotianArch <ebotian8964@gmail.com>
2026-07-02 20:32:51 +0800

    clean all

--- README.md
+++
@@ -1,5 +0,0 @@
-本项目采用 [GNU GPL v3](https://www.gnu.org/licenses/gpl-3.0.html) 协议开源。
-
-coco可以解锁所有
-# 个人博客
-
--- src/pages/coco.js
+++
@@ -1,8 +0,0 @@
-import Home, { getStaticProps } from "./index";
-
-export default function CocoPage(props) {
-	// 强制展示全部 posts
-	return <Home {...props} showAllOverride={true} />;
-}
-
-export { getStaticProps };
